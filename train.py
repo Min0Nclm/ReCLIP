3084b2e27d7cfae477eac51fe4d3b880fa9540d5
Min0Nclm <minodxck@gmail.com>
2025-08-22 13:40:14 +0800

    lr=0.0001

--- train.py
+++ train.py
@@ -82,10 +82,10 @@ def main(args):
 
     map_maker = MapMaker(image_size=args.config.image_size).to(model.device)
 
-    optimizer = torch.optim.Adam([
-            {'params': prompt_maker.prompt_learner.parameters(),'lr': 0.001},
-            {'params': adapter.parameters(),"lr":0.001},
-        ], lr=0.001, betas=(0.5, 0.999))
+        optimizer = torch.optim.Adam([
+            {'params': prompt_maker.prompt_learner.parameters(),'lr': 0.0001},
+            {'params': adapter.parameters(),"lr":0.0001},
+        ], lr=0.0001, betas=(0.5, 0.999))
 
     train_dataset = TrainDataset(args=args.config,
                                     source=os.path.join(args.config.data_root,args.config.train_dataset),
