a137d9d183854334a86906c9030cacdb63059831
Min0Nclm <minodxck@gmail.com>
2025-09-09 23:51:17 +0800

    debug

--- train.py
+++ train.py
@@ -488,10 +488,4 @@ if __name__ == '__main__':
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-    main(args) = seed
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
     main(args)
